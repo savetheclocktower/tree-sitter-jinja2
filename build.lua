4efe22f553f3859658b39cab26b8ecd3ac7d78a1
Thomas Scholtes <geigerzaehler@axiom.fm>
2025-01-08 10:34:26 +0100

    Run stylua on codebase

--- build.lua
+++ build.lua
@@ -1,6 +1,5 @@
 -- Neovim plugin build script
 -- See https://github.com/folke/lazy.nvim#-plugin-spec
 
-
 require("tree-sitter-jinja2").register_parser()
-vim.cmd('TSInstall! jinja2')
+vim.cmd("TSInstall! jinja2")

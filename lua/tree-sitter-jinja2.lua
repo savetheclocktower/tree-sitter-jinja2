d42d8830fe600d55baf034f5096f27916e501ad5
Dan Sully <dan+github@sully.org>
2025-01-08 01:03:42 -0800

    fix: compatibility with nvim-treesitter 1.x/main (#2)
    nvim-treesitter is undergoing a rewrite on the 'main' branch.
    
    This change adds compatibility with it - `get_parser_configs()` no longer
    exists, and `nvim-treesitter.parsers` is just a table.
    
    Add a stylua.toml config file as well for formatting consistency.

--- lua/tree-sitter-jinja2.lua
+++ lua/tree-sitter-jinja2.lua
@@ -1,16 +1,34 @@
 local M = {}
 
-
-local install_info = {
-  url = "https://github.com/geigerzaehler/tree-sitter-jinja2",
-  files = { "src/parser.c" },
-  branch = "main",
-}
-
 function M.register_parser()
-  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  local parsers = require("nvim-treesitter.parsers")
+
+  -- Register the plugin path as a tree-sitter parser to nvim-treesitter.
+  -- This is not required, but it allows nvim-treesitter to manage it e.g. :TS{Install,Update} jinja2
+  local file = debug.getinfo(1).source:match("@(.*/)")
+  local plugin_dir = vim.fn.fnamemodify(file, ":p:h:h")
+
+  --- @type ParserInfo[]
+  local parser_config
+
+  if type(parsers.get_parser_configs) == "function" then
+    parser_config = parsers.get_parser_configs()
+  else
+    -- "nvim-treesitter.parsers" exports the parser table itself starting nvim-treesitter v1.x
+    parser_config = parsers
+  end
+
+  if type(parser_config.jinja2) == "table" then
+    -- A jinja2 parser is already registered. Avoid overwriting it.
+    return
+  end
+
   parser_config.jinja2 = {
-    install_info = install_info,
+    install_info = {
+      url = plugin_dir,
+      files = { "src/parser.c" },
+      branch = "main",
+    },
     filetype = "jinja2",
   }
 end
@@ -20,7 +38,7 @@ function M.init()
 
   vim.treesitter.language.add("htmljinja", {
     path = vim.api.nvim_get_runtime_file("parser/jinja2.so", false)[1],
-    symbol_name = "jinja2"
+    symbol_name = "jinja2",
   })
 
   vim.filetype.add({
@@ -34,7 +52,7 @@ function M.init()
       [".*%.html%.j2"] = "htmljinja",
       [".*%.html%.jinja"] = "htmljinja",
       [".*%.html%.jinja2"] = "htmljinja",
-    }
+    },
   })
 end
 

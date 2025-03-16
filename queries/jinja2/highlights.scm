58120d616b887d7bfabe713e4885e8918ab7009b
Andrew Dupont <github@andrewdupont.net>
2025-03-16 12:50:47 -0700

    Fix `highlights.scm`

--- queries/jinja2/highlights.scm
+++ queries/jinja2/highlights.scm
@@ -7,14 +7,45 @@
 ] @string
 
 [
-  "{{"
-  "}}"
-  "statement_start"
-  "statement_end"
-  "raw_start"
-  "raw_end"
+  (extends_statement)
+  (block_start_statement)
+  (if_start_statement)
+  (if_end_statement)
+  (else_statement)
+  (elif_statement)
+  (for_start_statement)
+  (for_else_statement)
+  (for_end_statement)
+  (macro_start_statement)
+  (macro_end_statement)
+  (with_start_statement)
+  (with_end_statement)
+  (block_start_statement)
+  (block_end_statement)
 ] @macro
 
 [
-  "_keyword"
+  "block"
+  "endblock"
+  "call"
+  "endcall"
+  "elif"
+  "else"
+  "extends"
+  "filter"
+  "endfilter"
+  "for"
+  "endfor"
+  "if"
+  "endif"
+  "include"
+  "import"
+  "macro"
+  "endmacro"
+  "raw"
+  "endraw"
+  "set"
+  "endset"
+  "with"
+  "endwith"
 ] @keyword
